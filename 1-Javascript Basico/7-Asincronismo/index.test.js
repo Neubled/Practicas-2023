ad37948f3e8217e9832ee8f313de1b3876a4aa2b
Martin Delbueno <05tincho123@gmail.com>
2023-04-25 16:29:01 -0300

    Practicas-2023

--- 1-Javascript Basico/7-Asincronismo/index.test.js
+++ 1-Javascript Basico/7-Asincronismo/index.test.js
@@ -6,37 +6,64 @@ describe("asincronismo", () => {
      * Cree una promesa que se cumpla pasados 3seg
      */
 
-    const promise = new Promise();
+    const Promise = new Promise(function (resolve, reject) {
+      if (Math.random > 0.5) resolve("success");
+      else reject("juan");
+      
+     
+      setTimeout(function() {
+        Promise.then((res)) > console.log((res)).catch((arr)) > console.log((arr));
+      }, 3000 );
+     
 
+    });
+  
     promise.then((res) => expect(res).toBe("success"));
   });
-  it("Async Await", async () => {
+
+
     /**
      * Consuma la misma promesa del test anterior pero utilizando async await y almacene el resultado en 'res'
      */
+    it("Promesas", async () => {
+    const promise = new Promise(function (resolve, reject) {
+      if (Math.random > 0.5) resolve("success");
+      else reject("juan");
+   
+      setTimeout(function() {
+        if (Math.random() > 0.5) resolve("success");
+        else reject("juan");
+      }, 3000);
+    });
 
-    const promise = new Promise();
-
-    let res;
+    let res= await promise;;
 
     expect(res).toBe("success");
   });
+
+
+
   it("Consumo de API", async () => {
     /**
      * Use fetch() para consumir la api de "https://www.thecocktaildb.com/" y consulte la informacion del trago "Gin and Tonic"
      * NOTA: para buscar el trago por nombre utilice el siguienre parametro de busqueda: "Gin%20And%20Tonic"
      */
 
-    const URL = "https://www.thecocktaildb.com/api/json/v1/1/";
+    const URL = "https://www.thecocktaildb.com/drink/11403=${drink}";
+
+   const drink= "Gin%20And%20Tonic";
+
 
     // response
-    let res;
+    let res= await fetch (URL);
     // cuerpo de la response. res.json()
-    let data;
-
+    let data = await res.json();
+    data = data.drinks [0]
+    console.log(data)
     expect(data.idDrink).toBe("11403");
     expect(data.strDrink).toBe("Gin And Tonic");
   });
+
   it("Manejo de expeciones", async () => {
     /**
      * En caso que la promesa fracase guarde ":(" en la variable res. En caso de exito guarde ":)". Idem en caso de exito. Use Async Await.
@@ -45,9 +72,10 @@ describe("asincronismo", () => {
     const promise = new Promise((resolve, reject) => {
       Math.random() > 0.5 ? resolve("Buena suerte") : reject("Mala suerte");
     });
-
+  
     const res = "";
-
+  
     expect(res == ":)" || res == ":(").toBe(true);
   });
+
 });
